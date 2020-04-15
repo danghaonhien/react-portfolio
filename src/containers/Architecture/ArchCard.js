1df42ec01978ba3ae610a33f3e688a68dc32d653
Nhien Dang <danghaonhien@gmail.com>
2020-04-14 19:35:43 -0700

    add portfolio

---
+++ src/containers/Architecture/ArchCard.js
@@ -0,0 +1,43 @@
+import React from "react";
+import { makeStyles } from "@material-ui/core/styles";
+import Card from "@material-ui/core/Card";
+import CardActionArea from "@material-ui/core/CardActionArea";
+import CardActions from "@material-ui/core/CardActions";
+import CardContent from "@material-ui/core/CardContent";
+import CardMedia from "@material-ui/core/CardMedia";
+import Typography from "@material-ui/core/Typography";
+
+const useStyles = makeStyles({
+  root: {
+    fontFamily: "Gotu, san-serif",
+  },
+  media: {
+    height: 450,
+  },
+});
+
+export default function ArchCard(props) {
+  const classes = useStyles();
+
+  return (
+    <Card className={classes.root}>
+      <CardActionArea>
+        <CardMedia
+          className={classes.media}
+          component='img'
+          image={props.img}
+        />
+        <CardContent>
+          <Typography variant='h6' component='h6'>
+            {props.title}
+          </Typography>
+
+          <Typography variant='body2' color='textSecondary' component='p'>
+            {props.content}
+          </Typography>
+        </CardContent>
+      </CardActionArea>
+      <CardActions></CardActions>
+    </Card>
+  );
+}
--- src/containers/Architecture/index.js
+++ src/containers/Architecture/index.js
@@ -1,9 +1,14 @@
-import React, { Component } from "react";
+import React from "react";
 import { makeStyles } from "@material-ui/core/styles";
 import Paper from "@material-ui/core/Paper";
 import Grid from "@material-ui/core/Grid";
 import projects from "./../Portfolio/projects";
+import ArchCard from "./ArchCard";
+import Swiper from "react-id-swiper";
+import "swiper/css/swiper.css";
+import Zoom from "@material-ui/core/Zoom";
 import StyledCard from "./../Landing/StyledCard";
+import ScheduleIcons from "../../components/Icons/ScheduleIcons";
 const useStyles = makeStyles((theme) => ({
   root: {
     flexGrow: 1,
@@ -16,21 +21,121 @@ const useStyles = makeStyles((theme) => ({
 }));
 const Architecture = (props) => {
   const classes = useStyles();
+  const params = {
+    effect: "coverflow",
+    grabCursor: true,
+    centeredSlides: true,
+    slidesPerView: "auto",
 
+    coverflowEffect: {
+      rotate: 50,
+      stretch: 0,
+      depth: 150,
+      modifier: 1,
+      slideShadows: true,
+    },
+    pagination: {
+      el: ".swiper-pagination.customized-swiper-pagination",
+      type: "bullets",
+      clickable: true,
+    },
+    navigation: {
+      nextEl: ".swiper-button-next.customized-swiper-button-next",
+      prevEl: ".swiper-button-prev.customized-swiper-button-prev",
+    },
+
+    spaceBetween: 30,
+  };
   return (
-    <div className={classes.root}>
-      <Grid container spacing={3}>
-        <Grid item xs>
-          <Paper className={classes.paper}>
-            {" "}
-            <StyledCard
-              title={projects[0].title}
-              img={projects[0].imgURL}
-              content={projects[0].content}
-            />
-          </Paper>
+    <div>
+      <div className={classes.root}>
+        <Grid container spacing={3}>
+          <Grid item xs>
+            <Paper className={classes.paper} elevation={1}>
+              {" "}
+              <Swiper {...params}>
+                <div>
+                  <ArchCard
+                    title={projects[0].title}
+                    img={projects[0].imgURL}
+                    content={projects[0].content}
+                  />
+                </div>
+                <div>
+                  <ArchCard
+                    title={projects[1].title}
+                    img={projects[1].imgURL}
+                    content={projects[1].content}
+                  />
+                </div>
+                <div>
+                  <ArchCard
+                    title={projects[2].title}
+                    img={projects[2].imgURL}
+                    content={projects[2].content}
+                  />
+                </div>
+              </Swiper>
+            </Paper>
+          </Grid>
         </Grid>
-      </Grid>
+      </div>
+      <div className='icon-spin'>
+        <ScheduleIcons />
+      </div>
+
+      <div>
+        <Zoom in={true}>
+          <div className='landing-projects'>
+            <div className={classes.root}>
+              <Grid
+                container
+                spacing={5}
+                alignItems='center'
+                alignContent='center'
+              >
+                <Grid item xs={12} sm={12} md={6} lg={6} xl={6}>
+                  <Paper className={classes.paper} elevation={1}>
+                    <StyledCard
+                      title={projects[0].title}
+                      img={projects[0].imgURL}
+                      content={projects[0].content}
+                    />
+                  </Paper>
+                </Grid>
+                <Grid item xs={12} sm={12} md={6} lg={6} xl={6}>
+                  <Paper className={classes.paper} elevation={1}>
+                    <StyledCard
+                      title={projects[1].title}
+                      img={projects[1].imgURL}
+                      content={projects[1].content}
+                    />
+                  </Paper>
+                </Grid>
+                <Grid item xs={12} sm={12} md={6} lg={6} xl={6}>
+                  <Paper className={classes.paper} elevation={1}>
+                    <StyledCard
+                      title={projects[2].title}
+                      img={projects[2].imgURL}
+                      content={projects[2].content}
+                    />
+                  </Paper>
+                </Grid>
+                <Grid item xs={12} sm={12} md={6} lg={6} xl={6}>
+                  <Paper className={classes.paper} elevation={1}>
+                    <StyledCard
+                      title={projects[3].title}
+                      img={projects[3].imgURL}
+                      content={projects[3].content}
+                    />
+                  </Paper>
+                </Grid>
+              </Grid>
+            </div>
+          </div>
+        </Zoom>
+        ;
+      </div>
     </div>
   );
 };
